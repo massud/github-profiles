15bb70daf4cbdfb2dc504a8fbd8969bf038d2a89
massud <massudgnan@gmail.com>
2015-04-27 19:14:17 +0100

    grunt installed

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt){
+
+  grunt.initConfig({
+    jshint: {
+      all: ['Gruntfile.js', 'src/**/*.js', 'spec/**/*.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  grunt.registerTask('default', ['jshint', 'jasmine']);
+
+};
