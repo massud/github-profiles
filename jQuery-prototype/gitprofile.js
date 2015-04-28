c0b8ffbe3018f04a60eeb374fe4fe8a2d698e6de
massud <massudgnan@gmail.com>
2015-04-28 14:13:53 +0100

    jQuery proto

---
+++ jQuery-prototype/gitprofile.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+
+  $('.gitprofile').on('submit', function(e) {
+
+    e.preventDefault();
+
+    var url = 'https://api.github.com/users/' + $('input.username').val();
+
+    var template = $('template').html();
+
+    $.get(url, function(info) {
+      $('.container').prepend(Mustache.render(template, info));
+    }).fail(function() {
+      $('.container').prepend("User not found");
+    }).always(function() {
+      $('input.username').val('');
+      console.log(info)
+      });
+  });
+});
