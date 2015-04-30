de1c281ff000172afa03aeb975205173665d0335
massud <massudgnan@gmail.com>
2015-04-30 17:35:29 +0100

    completed protractor walkthrough

---
+++ test/e2e/githubProfileFeature.js
@@ -0,0 +1,23 @@
+describe('GitHub profile finder', function() {
+
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+  var searchButton = element(by.className('btn'))
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  })
+  
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('Github user search');
+  });
+
+  it('finds the last Spike', function() {
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.last().getText()).toEqual('spike01');
+  });
+
+
+});
